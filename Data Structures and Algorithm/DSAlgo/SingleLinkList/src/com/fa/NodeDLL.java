56ace30bdc1d48ebd2bc0fcd47574fd5cd88dfa6
Farazulhaque <farazattari786@gmail.com>
2021-11-08 19:45:53 +0530

    double Linked List nodes

---
+++ Data Structures and Algorithm/DSAlgo/SingleLinkList/src/com/fa/NodeDLL.java
@@ -0,0 +1,14 @@
+package com.fa;
+
+public class NodeDLL {
+    int data;
+	NodeDLL next_link;
+	NodeDLL prev_link;
+
+
+	public NodeDLL(int data) {
+		this.data = data;
+		next_link = null;
+		prev_link = null;
+	}
+}
