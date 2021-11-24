fdd10c6a07abbfcdbec3f0952a98a4edf6836d0d
Farazulhaque <farazattari786@gmail.com>
2021-11-24 18:02:43 +0530

    Assignment - 3 | Question - 3 A) Create an array of 4 products and populate it with objects of products by adding an accept() method B) Add the method displayAllProducts() by passing array as a parameter i.e., public void displayAllEmploees(Product[ ] products)

--- Object Oriented Programming with Java/JavaProgramming/src/assignments/ProductEntity.java
+++ Object Oriented Programming with Java/JavaProgramming/src/assignments/ProductEntity.java
@@ -5,11 +5,9 @@ class Product {
     String productName;
     float productPrice;
 
-    public Product(String productName, float productPrice) {
+    public Product() {
         this.productId = (int) (Math.random() * (9999 - 1111 + 1) + 1111);
         // To generate 4 digit random number
-        this.productName = productName;
-        this.productPrice = productPrice;
     }
 
     public int getProductId() {
@@ -29,11 +27,34 @@ class Product {
         System.out.printf("%-15s: %s\n", "Product Name", productName);
         System.out.printf("%-15s: %s\n", "Product Price", productPrice);
     }
+
+    public void accept(String productName, float productPrice) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+    }
+
+    public void displayAllProducts(Product[] products) {
+        for (int i = 0; i < products.length; i++) {
+            System.out.printf("%-15s: %s\n", "Product ID", products[i].getProductId());
+            System.out.printf("%-15s: %s\n", "Product Name", products[i].getProductName());
+            System.out.printf("%-15s: %s\n", "Product Price", products[i].getProductPrice());
+            System.out.println();
+        }
+    }
 }
 
 public class ProductEntity {
     public static void main(String[] args) {
-        Product p = new Product("Sugar", 50);
-        p.display();
+        Product[] p;
+        p = new Product[4];
+        for (int i = 0; i < 4; i++) {
+            p[i] = new Product();
+        }
+        p[0].accept("Sugar", 50);
+        p[1].accept("Salt", 20);
+        p[2].accept("Rice", 40);
+        p[3].accept("Oil", 80);
+        Product obj = new Product();
+        obj.displayAllProducts(p);
     }
 }
