16650b129278b20fc79eaec133349de57774c5a2
Farazulhaque <farazattari786@gmail.com>
2021-12-21 19:58:39 +0530

    1. Write a program to print even number from 1 to 20

--- Web_Programming_Technologies/Assignments/Assignment2/even.js
+++ Web_Programming_Technologies/Assignments/Assignment2/even.js
@@ -1,4 +1,4 @@
-// 1.Write a program to print even number from 1 to 20
+// 1. Write a program to print even number from 1 to 20
 function print_even() {
     for (var i = 1; i < 20; i++) {
         if (i % 2 == 0)
