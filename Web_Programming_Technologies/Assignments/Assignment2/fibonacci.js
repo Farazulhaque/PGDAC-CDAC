122c4ff2af4f74e529ab8cfc3872f078370f421a
Farazulhaque <farazattari786@gmail.com>
2021-12-21 19:57:04 +0530

    2. Write a code to print fibonnaci series( 1,1,2,3,5,8,13,21...) till 100.

--- Web_Programming_Technologies/Assignments/Assignment2/fibonacci.js
+++ Web_Programming_Technologies/Assignments/Assignment2/fibonacci.js
@@ -1,4 +1,4 @@
-// 2.Write a code to print fibonnaci series( 1,1,2,3,5,8,13,21...) till 100.
+// 2. Write a code to print fibonnaci series( 1,1,2,3,5,8,13,21...) till 100.
 function print_fibonacci_series() {
     var a = 1;
     var b = 1;
