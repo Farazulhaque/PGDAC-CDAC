a6976df35feab09d387c407d2a4a27299062d4bc
Farazulhaque <farazattari786@gmail.com>
2021-12-21 19:48:37 +0530

    5.Write a code to take string input from user and remove duplicate words.

---
+++ Web_Programming_Technologies/Assignments/Assignment2/duplicates.js
@@ -0,0 +1,15 @@
+// 5.Write a code to take string input from user and remove duplicate words.
+function remove_duplicates(str) {
+    var originalArr = str.split(" ");
+    var uniqueArr = [];
+
+    for (var i = 0; i < originalArr.length; i++) {
+        if (uniqueArr.indexOf(originalArr[i]) == -1) {
+            uniqueArr.push(originalArr[i]);
+        }
+    }
+    document.write(uniqueArr.join(" "));
+}
+
+var str = prompt("Enter a senetence");
+remove_duplicates(str);
