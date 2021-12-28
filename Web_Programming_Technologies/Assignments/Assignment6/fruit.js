3e00538c2447ade4735fb5c201e07eae8945f0f4
Farazulhaque <farazattari786@gmail.com>
2021-12-28 13:26:17 +0530

    Create a Fruit shop billing application ->Add fruit (id,name ,rate) ->Display fruit Add fruit option will take data from prompt User can enter fruit name and quantity in input boxes. If fruit is in list find the rate and print the total price on screen.

---
+++ Web_Programming_Technologies/Assignments/Assignment6/fruit.js
@@ -0,0 +1,39 @@
+// Create a Fruit shop billing application
+// ->Add fruit (id,name ,rate)
+// ->Display fruit
+// Add fruit option will take data from prompt
+// User can enter fruit name and quantity in input boxes. If fruit is in list find the rate and print the total price on screen.
+
+fruit_details = []
+$(document).ready(function () {
+    $("#add_fruit_btn").click(function () {
+        var id = prompt("Enter ID");
+        var name = prompt("Enter Name")
+        var rate = prompt("Enter Rate")
+
+        fruit_details.push({
+            id: id,
+            name: name,
+            rate: rate
+        })
+        alert("Fruit Added Successfully!!");
+    })
+
+    $("#display_fruit_btn").click(function () {
+        var name = prompt("Enter fruit name");
+        var qty = prompt("Enter quantity")
+
+        var found = false;
+        for (var i = 0; i < fruit_details.length; i++) {
+            if (fruit_details[i].name == name) {
+                found = true;
+                document.write("Fruit Name: " + name + "<br>");
+                document.write("Fruit Price: " + fruit_details[i].rate + "<br>");
+                document.write("Total Price: " + (parseFloat(fruit_details[i].rate) * qty));
+            }
+        }
+        if (found == false) {
+            alert("Fruit not found!!!!");
+        }
+    })
+})
