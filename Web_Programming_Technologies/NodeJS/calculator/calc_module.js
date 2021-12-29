0e07479b2f9cf699f29e6268e889702910c4dcb8
Farazulhaque <farazattari786@gmail.com>
2021-12-29 19:02:44 +0530

    nodejs

---
+++ Web_Programming_Technologies/NodeJS/calculator/calc_module.js
@@ -0,0 +1,9 @@
+exports.add = function(a,b){
+    return a + b;
+}
+exports.sub = function(a,b){
+    return a - b;
+}
+
+exports.name = "Faraz"
+// exports here allows the acces of this outside
---
+++ Web_Programming_Technologies/NodeJS/calculator/calculator.js
@@ -0,0 +1,6 @@
+var calc = require("./calc_module")
+// don't use .js with path
+
+console.log(calc.add(2, 3))
+console.log(calc.sub(3, 4))
+console.log(calc.name)
