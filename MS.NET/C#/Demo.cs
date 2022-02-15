7e1e8c27301ef2e28146d2ec62fa5ea1dbb3ea68
Farazulhaque <farazattari786@gmail.com>
2022-02-15 14:01:02 +0530

    first class of c#

---
+++ MS.NET/C#/Demo.cs
@@ -0,0 +1,24 @@
+// importing System library
+using System;
+class DemoClass1
+{
+    int id;
+    string name;
+    float salary;
+    static void Main(string[] args)
+    {
+        int x = 20;
+        string msg = "Working with c# Language";
+        System.Console.WriteLine("First C# Program");
+        System.Console.WriteLine(x);
+        System.Console.WriteLine(msg);
+    }
+}
+
+class DemoClass2
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("First C# Program");
+    }
+}
