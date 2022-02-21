b4ab181def9bf109d2dd253f8e8b538b6904a032
Farazulhaque <farazattari786@gmail.com>
2022-02-21 19:09:15 +0530

    using indexer in 2d array and math method and regex methods

--- MS.NET Technologies/C#/ConsoleDemoApp/Day7/Assignment3/Assignment3.cs
+++ MS.NET Technologies/C#/ConsoleDemoApp/Day7/Assignment3/Assignment3.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ConsoleDemoApp.Day7.Assignment3
@@ -14,6 +15,27 @@ namespace ConsoleDemoApp.Day7.Assignment3
             Console.WriteLine("\n=====================\n");
             GetFrame(4, 5, "#");
             Console.WriteLine("\n=====================\n");
+            ToCamelCase("hello_csharp");
+            ToSnakeCase("helloCsharp");
+            ToCamelCase("is_modal_open");
+            ToSnakeCase("getColor");
+            Console.WriteLine("\n=====================\n");
+            Console.WriteLine($"Math.PI: {Math.PI}");
+            Console.WriteLine($"Math.Round(100.4): {Math.Round(100.4)}");
+            Console.WriteLine($"Math.Round(100.6): {Math.Round(100.6)}");
+            Console.WriteLine($"Math.Floor(100.4): {Math.Floor(100.4)}");
+            Console.WriteLine($"Math.Floor(100.6): {Math.Floor(100.6)}");
+            Console.WriteLine($"Math.Ceiling(100.4): {Math.Ceiling(100.4)}");
+            Console.WriteLine($"Math.Ceiling(100.6): {Math.Ceiling(100.6)}");
+            Console.WriteLine($"Math.Sqrt(25): {Math.Sqrt(25)}");
+            Console.WriteLine($"Math.Truncate(110.52): {Math.Truncate(110.52)}");
+            Console.WriteLine($"Math.Truncate(110.32): {Math.Truncate(110.32)}");
+            Console.WriteLine($"Math.Math.Pow(3,4): {Math.Pow(3, 4)}");
+            Console.WriteLine($"Math.Sin(60): {Math.Sin(60)}");
+            Console.WriteLine($"Math.Cos(60): {Math.Cos(60)}");
+            Console.WriteLine($"Math.Tan(60): {Math.Tan(60)}");
+            Console.WriteLine($"Math.Round(100.4236,2): {Math.Round(100.4236, 2)}");
+            Console.WriteLine($"Math.Round(100.4456,2): {Math.Round(100.4456, 2)}");
         }
         static void FetchOutput(string str)
         {
@@ -45,7 +67,6 @@ namespace ConsoleDemoApp.Day7.Assignment3
             else
             {
                 Array frame = Array.CreateInstance(typeof(string), height);
-
                 for (int i = 0; i < height; i++)
                 {
                     for (int j = 0; j < width; j++)
@@ -54,18 +75,47 @@ namespace ConsoleDemoApp.Day7.Assignment3
                         {
                             Console.Write("#");
                         }
-
                         else if (j != 0 || j != width - 1)
                         {
                             Console.Write(" ");
                         }
                     }
                     Console.WriteLine();
-
                 }
             }
         }
-
+        static void ToCamelCase(string str)
+        {
+            string camel_case = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '_')
+                {
+                    camel_case += str[++i].ToString().ToUpperInvariant();
+                }
+                else
+                {
+                    camel_case += str[i];
+                }
+            }
+            Console.WriteLine(camel_case);
+        }
+        static void ToSnakeCase(string str)
+        {
+            string snake_case = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] >= 65 && str[i] <= 90)
+                {
+                    snake_case += "_" + str[i].ToString().ToLowerInvariant();
+                }
+                else
+                {
+                    snake_case += str[i];
+                }
+            }
+            Console.WriteLine(snake_case);
+        }
     }
 
 }
---
+++ MS.NET Technologies/C#/ConsoleDemoApp/Day7/Assignment3/IndexerFor2DArray.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDemoApp.Day7.Assignment3
+{
+    class Employee
+    {
+        string[,] FullName;
+        public Employee(int size = 5)
+        {
+            FullName = new string[size, 2];
+        }
+        public string this[int r, int c]
+        {
+            get { return FullName[r, c]; }
+            set { FullName[r, c] = value; }
+        }
+        public int ArrayLength
+        {
+            get => FullName.GetLength(0);
+        }
+        public void DisplayEmployees()
+        {
+            Console.WriteLine("Employee names are: ");
+            foreach (var item in FullName)
+            {
+                Console.WriteLine(item[0] + "\t" + item[1]);
+            }
+        }
+    }
+    internal class IndexerFor2DArray
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp[0, 0] = "Faraz";
+            emp[0, 1] = "Haque";
+            emp[1, 0] = "Arisha";
+            emp[1, 1] = "Aftab";
+            emp[2, 0] = "Syeda";
+            emp[2, 1] = "Kashaf";
+            Console.WriteLine("Reading all data using indexer: ");
+            Console.WriteLine(emp.ArrayLength);
+
+            for (int i = 0; i < emp.ArrayLength; i++)
+            {
+                Console.WriteLine(emp[i, 0] + "\t" + emp[i, 1]);
+            }
+
+
+        }
+    }
+}
