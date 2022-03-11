f53b57a0d71bfc6d3a724bb6bf6eaf8d8a495617
Farazulhaque <farazattari786@gmail.com>
2022-03-11 19:03:48 +0530

    EF Annotations,DB first Approach

---
+++ MS.NET Technologies/WebApp/DB_FirstEntity/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DB_FirstEntity
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ MS.NET Technologies/WebApp/DB_FirstEntity/Controllers/FacultiesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DB_FirstEntity.Models;
+
+namespace DB_FirstEntity.Controllers
+{
+    public class FacultiesController : Controller
+    {
+        private CollegeDbEntities1 db = new CollegeDbEntities1();
+
+        // GET: Faculties
+        public ActionResult Index()
+        {
+            var faculties = db.Faculties.Include(f => f.Standard);
+            return View(faculties.ToList());
+        }
+
+        // GET: Faculties/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Faculty faculty = db.Faculties.Find(id);
+            if (faculty == null)
+            {
+                return HttpNotFound();
+            }
+            return View(faculty);
+        }
+
+        // GET: Faculties/Create
+        public ActionResult Create()
+        {
+            ViewBag.StandardId = new SelectList(db.Standards, "StandardId", "StandardName");
+            return View();
+        }
+
+        // POST: Faculties/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "FacultyId,FacultyName,StandardId")] Faculty faculty)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Faculties.Add(faculty);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.StandardId = new SelectList(db.Standards, "StandardId", "StandardName", faculty.StandardId);
+            return View(faculty);
+        }
+
+        // GET: Faculties/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Faculty faculty = db.Faculties.Find(id);
+            if (faculty == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.StandardId = new SelectList(db.Standards, "StandardId", "StandardName", faculty.StandardId);
+            return View(faculty);
+        }
+
+        // POST: Faculties/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "FacultyId,FacultyName,StandardId")] Faculty faculty)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(faculty).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.StandardId = new SelectList(db.Standards, "StandardId", "StandardName", faculty.StandardId);
+            return View(faculty);
+        }
+
+        // GET: Faculties/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Faculty faculty = db.Faculties.Find(id);
+            if (faculty == null)
+            {
+                return HttpNotFound();
+            }
+            return View(faculty);
+        }
+
+        // POST: Faculties/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Faculty faculty = db.Faculties.Find(id);
+            db.Faculties.Remove(faculty);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
