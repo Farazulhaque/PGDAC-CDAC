83fec695a14f38bce7e46333ffcf5183cd930198
Farazulhaque <farazattari786@gmail.com>
2022-03-12 13:52:12 +0530

    using mvc architecture

---
+++ MS.NET Technologies/WebApp/DB_FirstEntity/Controllers/StateMgtController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DB_FirstEntity.Controllers
+{
+    public class Test
+    {
+        public int Id { get; set; }
+    }
+    public class StateMgtController : Controller
+    {
+        // GET: StateMgt
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Data from view data";
+            ViewData["Test"] = new Test() { Id = 2000 };
+            return View();
+        }
+    }
+}
---
+++ MS.NET Technologies/WebApp/DB_FirstEntity/Models/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DB_FirstEntity.Models
+{
+    //DTO (Data Transfer Object) Class
+    public class EmployeeDetails
+    {
+        // Employee POCO (Plain Old Clr Object)
+        public int EId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        //Address POCO
+        public string Addressline { get; set; }
+        public string state { get; set; }
+        public string city { get; set; }
+    }
+}
